3fc6892f3256cad9c0e19865411064c1cc20f578
Abumuhab <abumuhab98@gmail.com>
2020-12-11 20:58:23 +0100

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,8 @@
 const admin = require('firebase-admin')
-const functions = require('firebase-functions')
 admin.initializeApp()
 const transactionController = require('./controllers/transactction')
 const authController = require('./controllers/auth')
 const taskController = require('./controllers/task')
-const random = require('./utils/random')
 
 // transactions
 exports.initializeTransaction = transactionController.initializeTransaction
@@ -15,21 +13,3 @@ exports.signUp = authController.signUp
 
 // task assignment
 exports.getDeliveryTask = taskController.getDeliveryTask
-
-// eidts
-exports.edit = functions.https.onRequest((req, res) => {
-    admin.firestore().collection('orders').get().then(querySnap => {
-        const batch = admin.firestore().batch()
-        querySnap.docs.forEach(doc => {
-            batch.update(doc.ref, {
-                pickupVerificationCode: random.generateRandomId(),
-                dropoffVerificationCode: random.generateRandomId()
-            })
-        })
-        batch.commit().then(val => {
-            return res.json({
-                successful: true
-            })
-        })
-    })
-})
