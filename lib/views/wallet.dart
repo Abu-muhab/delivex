5a2966c3c34a20757a332b31f867b7445bd08bba
Abumuhab <abumuhab98@gmail.com>
2020-12-14 16:20:13 +0100

    Update wallet.dart

--- lib/views/wallet.dart
+++ lib/views/wallet.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:node_auth/constants/colors.dart';
+import 'package:intl/intl.dart';
 
 class WalletPage extends StatefulWidget {
   State createState() => WalletPageState();
@@ -8,8 +9,61 @@ class WalletPage extends StatefulWidget {
 class WalletPageState extends State<WalletPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: kLeichtPrimaryColor,
+    return LayoutBuilder(
+      builder: (context, constraint) {
+        return Container(
+          color: kLeichtPrimaryColor,
+          child: Stack(
+            children: [
+              Align(
+                alignment: Alignment.topCenter,
+                child: Container(
+                  height: constraint.maxHeight * 0.3,
+                  child: SafeArea(
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 15, bottom: 15),
+                      child: Center(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('Wallet Balance',
+                                style: TextStyle(
+                                    color: Colors.grey[300],
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.w500)),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Text(
+                                '₦ ${NumberFormat("#,###.##", "en_US").format(2000000)}',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 40,
+                                    fontWeight: FontWeight.bold))
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  height: constraint.maxHeight * 0.7,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30),
+                      )),
+                ),
+              )
+            ],
+          ),
+        );
+      },
     );
   }
 }
